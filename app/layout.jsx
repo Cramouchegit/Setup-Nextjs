b994ea54e42476752d793d29099ca92733ab05bf
Norman <norman@advancedgroup.com>
2024-11-18 01:40:00 +0700

    create: Setup Nextjs Manual Installation

---
+++ app/layout.jsx
@@ -0,0 +1,10 @@
+export default function Layout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Belajar NextJS Fundamental</title>
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
